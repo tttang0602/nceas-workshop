041431752b70be0f93727dd53bfa061045cb9449
Tang <yiyit62@gmail.com>
2017-07-19 17:54:43 -0500

    add plot of seamap environment by year and by location

--- gom_incident.R
+++ gom_incident.R
@@ -2,6 +2,7 @@ datalibrary(ggplot)
 library(ggmap)
 library(maps)
 library(mapdata)
+library(tidyverse)
 #find boundary of data set
 incident<-read_csv("Incidents_NOAA1985-2016.csv")
 incident.pre<-incident[!is.na(incident$lat),]
@@ -13,8 +14,6 @@ lat3<-lat3[lat3$lon> -100,]
 map.box<- make_bbox(lon = lat3$lon,lat = lat3$lat)
 
 
-
-
 # grab map from google
 my.map<-get_map(location = map.box,source = "google",maptype = "terrain")
 
@@ -23,3 +22,28 @@ ggmap(my.map)+geom_point(data=lat3,mapping = aes(x=lat3$lon,y=lat3$lat,
                                                          color=lat3$max_ptl_release_gallons))
 
 
+##map the environment measurements in SEAMAP
+location<- read.csv("sta_env.csv")
+year<- read.table("CRUISES.txt",sep = ",",header = TRUE)
+year<-data.frame(na.omit(year$CRUISEID),na.omit(year$YR))
+location<-location[!is.na(location$DECSLAT),]
+locat<-data.frame((location$DECSLAT),(location$DECSLON),(location$CRUISEID))
+colnames(year)<-c("CRUISEID","YR")
+colnames(locat)<-c("lat","lon","CRUISEID")
+loc_yr<-inner_join(year,locat,by="CRUISEID")
+
+#map.box<- make_bbox(lon = locat$lon,lat = locat$lat)
+map.box[1]<- -103
+map.box[2]<-10
+map.box[3]<- -70
+map.box[4]<-35
+my.map<-get_map(location = map.box,source = "google",maptype = "terrain")
+ggmap(my.map)+geom_point(data=location,mapping = aes(x=location$DECSLON,y=location$DECSLAT))
+
+measure.year<-loc_yr%>%group_by(YR) %>% count()
+
+p <- ggplot(measure.year, aes(YR, n)) + geom_bar(
+  stat = "identity", 
+  fill = "forestgreen", 
+  width = 0.25) + coord_flip()
+p
